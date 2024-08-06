b4e01767fbfed50663114e8b46023c812ae52306
Vaidic Jain <97245014+VaidicJain@users.noreply.github.com>
2024-08-06 21:22:25 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
+import Image from "next/image";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import Clients from "@/components/Clients";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+import HeroMain from "@/components/HeroParallax";
+
+export default function Home() {
+  return (
+    <main className="relative  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <div className="max-w-7xl w-full">
+      <Hero/>
+      <FloatingNav navItems={navItems} />
+      <HeroMain/>
+      <Grid />
+      {/* <RecentProjects/> */}
+      <Clients/>
+      {/* <Experience /> */}
+      <Approach/>
+      <Footer />
+    </div>
+  </main>
+  );
+}
---
+++ components/HeroParallax.tsx
@@ -0,0 +1,103 @@
+"use client";
+import React from "react";
+import { HeroParallax } from "@/components/ui/Hero-Parallax";
+
+ const HeroParallaxDemo = ()=>{
+  return <HeroParallax products={products} />;
+}
+export const products = [
+  {
+    title: "EduConnect",
+    link: "#",
+    thumbnail:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiGAhu8v2L1KxW8FYeejOCV40h72IVSsQMIA&s",
+  },
+  {
+    title: "Cloud.24",
+    link: "#",
+    thumbnail: "https://uizard.io/static/89cb1d30cdee9c19b0ce72efe004ff49/a8e47/f5e31989f816c5b15c47cd54b0eb0b30b210b6f0-1440x835.png",
+  },
+  {
+    title: "Samvaad",
+    link: "#",
+    thumbnail:
+      "https://t3.ftcdn.net/jpg/04/40/55/08/360_F_440550842_axpix9gdXQlEM1MvoPFMt7dctHsF2OAd.jpg",
+  },
+
+//   {
+//     title: "Editorially",
+//     link: "https://editorially.org",
+//     thumbnail:
+//       "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+//   },
+//   {
+//     title: "Editrix AI",
+//     link: "https://editrix.ai",
+//     thumbnail:
+//       "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+//   },
+//   {
+//     title: "Pixel Perfect",
+//     link: "https://app.pixelperfect.quest",
+//     thumbnail:
+//       "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+//   },
+
+//   {
+//     title: "Algochurn",
+//     link: "https://algochurn.com",
+//     thumbnail:
+//       "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+//   },
+//   {
+//     title: "Aceternity UI",
+//     link: "https://ui.aceternity.com",
+//     thumbnail:
+//       "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+//   },
+//   {
+//     title: "Tailwind Master Kit",
+//     link: "https://tailwindmasterkit.com",
+//     thumbnail:
+//       "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+//   },
+//   {
+//     title: "SmartBridge",
+//     link: "https://smartbridgetech.com",
+//     thumbnail:
+//       "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+//   },
+  {
+    title: "UnderBelly",
+    link: "#",
+    thumbnail:
+      "https://assets.devfolio.co/projects/357b1d08f4b649af832318e262068605/pic30ho8ngrp.jpg",
+  },
+
+  {
+    title: "Indian Heritages",
+    link: "#",
+    thumbnail:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTftTttnhw9pN9hg-yM-_kfhqG4Tx97tvmmuehCPdHqvny0LPBbNeMr5sTk89NdwVYEfz8&usqp=CAU",
+  },
+  {
+    title: "AIZone",
+    link: "#",
+    thumbnail:
+      "https://cdn.dribbble.com/userupload/7325823/file/original-21286e81a7d84203ea0d4e65472f5104.png?resize=1504x1128",
+  },
+  {
+    title: "NewzMonkey",
+    link: "#",
+    thumbnail:
+      "https://colorlib.com/wp-content/uploads/sites/2/news-wordpress-themes-4.jpg",
+  },
+  {
+    title: "Rhythm",
+    link: "#",
+    thumbnail:
+      "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/c8594e167771655.642eac429bbf5.png",
+  },
+];
+
+export default HeroParallaxDemo
